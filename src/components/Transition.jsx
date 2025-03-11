4e8bb8374cb0dc7624ba266c6014d4c357c939f6
Coding rev <fernando.lobo.prez@gmail.com>
2025-03-11 15:30:44 -0300

    01-Transition basics
    01-Transition basics

--- src/components/Transition.jsx
+++ src/components/Transition.jsx
@@ -1,7 +1,35 @@
+import { useState, useRef } from "react";
+import { Transition } from "react-transition-group";
+
 const TransitionComp = () => {
+  const nodeRef = useRef(null);
+  let [show,setShow] = useState(true) 
+
+  const showDiv = () =>{
+    setShow(!show ? true:false)
+  }
+
   return (
     <>
-      <div>Transition</div>
+      <Transition
+        nodeRef={nodeRef}
+        in={show}
+        timeout={2000}
+      >
+        {state =>
+          <div
+            style={{
+                background:'red',
+                height:'100px',
+                transition:'all 2s ease',
+                opacity: state === 'exited' || state === 'exiting' ? 0:1
+            }}
+          >
+            {state}
+          </div>
+        }
+      </Transition>
+      <button className="btn btn-primary" onClick={showDiv}>Show</button>
     </>
   )
 };
